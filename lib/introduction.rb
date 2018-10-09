1485764beaee6e0a2032ff3a018aa27b4bac78f6
Matthew Sherman <msherman0801@gmail.com>
2018-10-09 23:11:52 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(x)
   puts "x"
 end
 
-def introduction_with_language(x,y)
-  puts `Does this phrase have to include #{x} or #{y}?`
+def introduction_with_language(name, language)
+  puts `Does this phrase have to include #{name} or #{language}?`
 end
